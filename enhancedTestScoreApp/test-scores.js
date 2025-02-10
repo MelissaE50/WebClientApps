5b51f65553ada98d76295140b97285af7f8ddd9e
MelissaE50 <126819117+MelissaE50@users.noreply.github.com>
2025-02-10 22:03:26 +0000

    working on enhanced test score app

--- enhancedTestScoreApp/test-scores.js
+++ enhancedTestScoreApp/test-scores.js
@@ -1,32 +1,39 @@
-let total = 0;
-let count = 0;
+// declare and initialize an array to hold test scores from user
+const scores = [];
+ 
+// use do-while loop to get scores from the user and store in array
 let score = 0;
-//let highestScore = 0;
- 
 do {
-    // get a score from the user
     score = parseInt(prompt(
-        "Enter a test score, or enter -1 to end scores.", -1));
+        "Enter a test score, or enter -1 to end scores", -1));
  
-    // if it's valid, add to total, increment count,
-    // and display score
     if (score >= 0 && score <= 100) {
-        total = total + score;    
-        count++;
-        document.write(`<p>Score ${count}: ${score}</p>`);
+        scores[scores.length] = score;
     }
-    // if user isn't ending scores, notify them of invalid data 
     else if (score != -1){
-        alert("Score must be a valid number from 0 through 100.");
+        alert("Score must by a valid number from 0 through 100");
     }
 }
 while(score != -1);
-//calculate and display average score
-const average = parseInt(total/count);
-document.write(`<p>Average score is ${average}</p>`);
+
+if (scores.length > 0) {
+    // use a for-in loop to add each score to total and display it
+    let total = 0;
+    for (let i in scores) {
+        total = total + scores[i];
+        document.write(
+            `<p>Score ${parseInt(i) + 1}: ${scores[i]}</p>`);
+    }
+ 
+    //calculate and display the average
+    const average = parseInt(total/scores.length);
+    document.write(`<p>Average score is ${average}</p>`);
+}
 
 let highestScore = 0;
+for (let val of scores) {  // val holds the current value
+  highestScore = val;                
+}
+document.write(`<p>Highest score is ${highestScore}</p>`); //display highest score
 
-//for-of loop here
-//document.write(`<p>Highest score is ${highestScore}</p>`);
 
