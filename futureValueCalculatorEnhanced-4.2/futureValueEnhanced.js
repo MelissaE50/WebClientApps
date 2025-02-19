d2391dade76555be10222fbf273f1450d2324fed
MelissaE50 <126819117+MelissaE50@users.noreply.github.com>
2025-02-19 20:51:03 +0000

    fix future value enhanced app

--- futureValueCalculatorEnhanced-4.2/futureValueEnhanced.js
+++ futureValueCalculatorEnhanced-4.2/futureValueEnhanced.js
@@ -5,9 +5,9 @@ const $ = selector => document.querySelector(selector);
 const calculateFV = (investment, rate, years) => {
     let futureValue = investment;
     for (let i = 1; i <= years; i++ ) {
-        futureValue += futureValue * rate / 100;
+        futureValue += futureValue * rate / 100; //for loop to take values
     }
-    futureValue = futureValue.toFixed(2);
+    futureValue = futureValue.toFixed(2); //calculate future value
     return futureValue;
 };
 
@@ -19,13 +19,13 @@ document.addEventListener("DOMContentLoaded", () => {
         let errorMessage = "";
     
         if (isNaN(investment) || investment <= 0 || investment > 100000) {
-            errorMessage = "Investment must be a number greater than 0 and less than or equal to 100,000";
+            errorMessage = "Investment must be a number greater than 0 and less than or equal to 100,000"; //create error message if number is less than zero or over 100,000
             $("#investment").focus();
         } else if (isNaN(rate) || rate <= 0 || rate > 15) {
-            errorMessage = "Rate must be a number greater than 0 and less than or equal to 15";
+            errorMessage = "Rate must be a number greater than 0 and less than or equal to 15"; //create error message if number is less than zero or over 15
             $("#rate").focus();
         } else if (isNaN(years) || years <= 0 || years > 50) {
-            errorMessage = "Years must be a number greater than 0 and less than or equal to 50";
+            errorMessage = "Years must be a number greater than 0 and less than or equal to 50"; //create error message if number is less than zero or over 50
             $("#years").focus();
         }
         if (errorMessage == "") {
