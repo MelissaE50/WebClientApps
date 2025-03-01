71bdf241b8d4b934411acc8127c7efea68394489
MelissaE50 <126819117+MelissaE50@users.noreply.github.com>
2025-03-01 02:44:53 +0000

    finish slide show assignment

--- slideShowApplication-7.2/slideShowapp.js
+++ slideShowApplication-7.2/slideShowapp.js
@@ -2,27 +2,53 @@
 
 const $ = selector => document.querySelector(selector);
 
+const imageCache = [];
+let imageCounter = 0;
+let timer = null;
+let image = null;
+
+const caption = $("#caption");        // the h2 element for the caption
+const mainImage = $("#main_image");   // the img element for the show
+
+const runSlideShow = () => {
+    imageCounter = (imageCounter +1) % imageCache.length;
+    image = imageCache[imageCounter];
+    mainImage.src = image.src;
+    mainImage.alt = image.alt;
+    caption.textContent = image.alt;
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-    
-    const caption = $("#caption");        // the h2 element for the caption
-    const mainImage = $("#main_image");   // the img element for the show
         
     // get all the <a> tags in the ul element
     const links = $("#image_list").querySelectorAll("a");
     
     // Process image links
-    const imageCache = [];
-    let image = null;
     for ( let link of links ) {
         // Preload image and copy title properties
         image = new Image();
         image.src = link.href;
         image.alt = link.title;
+
         // add image to array 
         imageCache[imageCache.length] = image;
     }
 
-    // Start slide show
+    //attach start and pause event handlers
+    $("#start").addEventListener("click", () => {
+        runSlideShow();
+        timer = setInterval(runSlideShow, 2000);
+        $("#start").disabled = true;
+        $("#pause").disabled = false;
+    });
+    $("#pause").addEventListener("click", () => {
+        clearInterval(timer);
+        $("#start").disabled = false;
+        $("#pause").disabled = true;
+    });
+
+});
+/* Start slide show
     let imageCounter = 0;
     setInterval( () => { 
         // calculate the index for the current image
@@ -34,5 +60,4 @@ document.addEventListener("DOMContentLoaded", () => {
         mainImage.alt = image.alt;
         caption.textContent = image.alt;
     },
-    2000);  // 2 second interval
-});
+    2000);  // 2 second interval */
