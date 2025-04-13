732f01aa8a260cf7c487d35a19f1ad1a0c25225d
MelissaE50 <126819117+MelissaE50@users.noreply.github.com>
2025-04-13 14:24:15 +0000

    work on 13.3

--- accountProfileAppEnhanced-13.3/accountProfile.js
+++ accountProfileAppEnhanced-13.3/accountProfile.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const isDate = (date, datePattern) => {
-    if (!datePattern.test(date)) { return false; }
+    if (!datePattern.test(date)) { return false; }}
+
+    if(type == "full"){
 
     const dateParts = date.split("/");
     const month = parseInt( dateParts[0] );
@@ -22,12 +24,16 @@ $( document ).ready( () => {
         const phone = $("#phone").val();
         const zip = $("#zip").val();
         const dob = $("#dob").val();
+        const card = $("#card").val();
+        const cc_date = $("#cc_date").val();
 
         // regular expressions for validity testing
         const emailPattern = /^[\w\.\-]+@[\w\.\-]+\.[a-zA-Z]+$/;
         const phonePattern = /^\d{3}-\d{3}-\d{4}$/;
         const zipPattern = /^\d{5}(-\d{4})?$/;
         const datePattern = /^[01]?\d\/[0-3]\d\/\d{4}$/;
+        const cardPattern = /^\d{4}-\d{4}-\d{4}-\d{4}$/;
+        const cc_DatePattern = /^[01]?\d\/\d{4}$/;
         
         // check user entries for validity
         let isValid = true;
@@ -46,6 +52,14 @@ $( document ).ready( () => {
         if ( dob === "" || !isDate(dob, datePattern) ) {
             isValid = false;
             $("#dob").next().text("Please enter a valid date in MM/DD/YYYY format.");
+        }
+        if ( card === "" || !cardPattern.test(card)) {
+            isValid = false;
+            $("#card").next().text("Please enter a credit card in NNNN-NNNN-NNNN-NNNN format.");
+        }
+        if ( cc-date === "" || !isDate(cc_date, cc_datePattern, "cc")) {
+            isValid = false;
+            $("#cc_date").next().text("Please enter a valid date in MM/YYYY format.");
         }
         
         if ( isValid ) { 
